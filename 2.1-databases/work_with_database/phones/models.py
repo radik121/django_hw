80542e7a45250891adbb0f47036314bbfb83b0af
Radik Masagutov <radik121@mail.ru>
2022-03-21 15:08:31 +0300

    add migrate

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50, unique=True)
+    price = models.IntegerField()
+    image = models.URLField(max_length=200)
+    release_date = models.DateField()
+    lte_exists = models.CharField(max_length=10)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    # def save(self, *args, **kwargs):
+    #     if self.slug is None:
+    #         self.slug = slugify(self.name)
+    #     super().save(*args, **kwargs)
