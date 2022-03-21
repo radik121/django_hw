1ebdd8f3938d792280130c8aa554273bee90423e
Radik Masagutov <radik121@mail.ru>
2022-03-21 15:09:44 +0300

    add catalog and product

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
+    catalog = Phone.objects.all()
+    sort = request.GET.get('sort')
+    if sort == 'name':
+        catalog = catalog.order_by('name')
+    if sort == 'min_price':
+        catalog = catalog.order_by('price')
+    if sort == 'max_price':
+        catalog = catalog.order_by('-price')
     template = 'catalog.html'
-    context = {}
+    context = {
+        'phones': [c for c in catalog]
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
+    phone = Phone.objects.get(slug=slug)
     template = 'product.html'
-    context = {}
+    context = {
+        'phone': phone
+    }
     return render(request, template, context)
