c7cec05ac47c425b5af7fbcc7e7fc1e51456ef09
Radik Masagutov <radik121@mail.ru>
2022-03-24 09:46:50 +0300

    add library

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from books.models import Book
+
+
+def index(request):
+    return redirect('books')
 
 
 def books_view(request):
+    catalog = Book.objects.all()
     template = 'books/books_list.html'
-    context = {}
+    context = {
+        'books': [c for c in catalog]
+    }
+    return render(request, template, context)
+
+
+def one_book_view(request, pub_date):
+    book = Book.objects.get(pub_date=pub_date)
+    try:
+        prev_date = Book.objects.order_by('-pub_date').filter(pub_date__lt=pub_date)[0:1].get()
+    except Book.DoesNotExist:
+        prev_date = Book.objects.order_by('-pub_date')[0:1].get()
+    try:
+        next_date = Book.objects.order_by('pub_date').filter(pub_date__gt=pub_date)[0:1].get()
+    except Book.DoesNotExist:
+        next_date = Book.objects.order_by('pub_date')[0:1].get()
+    template = 'books/one_book.html'
+    context = {
+        'book': book,
+        'prev_date': prev_date,
+        'next_date': next_date,
+    }
+    # print(prev_date)
     return render(request, template, context)
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -13,12 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from datetime import datetime
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, register_converter, re_path
+from books.views import books_view, index, one_book_view
 
-from books.views import books_view
+
+class DateConverter:
+    regex = r'[0-9]{4}-[0-9]{2}-[0-9]{2}'
+
+    def to_python(self, value: str) -> datetime:
+        return datetime.strptime(value, '%Y-%m-%d')
+
+    def to_url(self, value):
+        return value
+
+
+register_converter(DateConverter, 'yyyy-mm-dd')
 
 urlpatterns = [
-    path('', books_view, name='books'),
+    path('', index),
+    path('books/', books_view, name='books'),
+    path('books/<yyyy-mm-dd:pub_date>', one_book_view, name='one_book'),
+    # re_path('book/(?P<pub_date>[0-9]{4}-[0-9]{2}-[0-9]{2})', one_book_view, name='one_book'),
     path('admin/', admin.site.urls),
 ]
